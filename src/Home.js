6647634fcb3d7661f6384f66c35766dd6bde5465
kavyakonisa <kavyakonisa1234@gmail.com>
2021-02-07 21:01:22 +0530

    React Basics

---
+++ src/Home.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+const Home = () => {
+    // let name='mario';
+    const [name,setName] = useState('mario');
+    const [age,setAge]= useState(25);
+    
+    
+    const handleClick = () => {
+       setName('luigi');
+       setAge(38);
+    }
+
+
+    return (  
+        <div className="home">
+            <h2>Home Page</h2>
+            <p>{ name } is { age} years old</p>
+            <button onClick={handleClick}>Click Me</button>
+        </div>
+    );
+}
+ 
+export default Home;
